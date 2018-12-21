05d903aa61633beb9cbd51756bd4d9d92a5e73a1
seiruss <seiruss@users.noreply.github.com>
2018-12-20 22:39:19 -0600

    Fix spacing

--- examples/discard_sessions.rs
+++ examples/discard_sessions.rs
@@ -7,100 +7,100 @@ use std::process;
 use std::io::{self, Write};
 
 fn main() {
-	println!("Rust Management API Discard WEB_API Sessions Example\n");
+    println!("Rust Management API Discard WEB_API Sessions Example\n");
 
-	let mut client = build_client();
+    let mut client = build_client();
 
-	if let Err(e) = login(&mut client) {
-		eprintln!("Failed to login: {}", e);
-		process::exit(1);
-	}
+    if let Err(e) = login(&mut client) {
+        eprintln!("Failed to login: {}", e);
+        process::exit(1);
+    }
 
-	if let Err(e) = discard_sessions(&mut client) {
-		eprintln!("Failed to discard sessions: {}", e);
-		logout(&mut client).expect("Failed to logout");
-		process::exit(1);
-	}
+    if let Err(e) = discard_sessions(&mut client) {
+        eprintln!("Failed to discard sessions: {}", e);
+        logout(&mut client).expect("Failed to logout");
+        process::exit(1);
+    }
 
-	if let Err(e) = logout(&mut client) {
-		eprintln!("Failed to logout: {}", e);
-		process::exit(1);
-	}
+    if let Err(e) = logout(&mut client) {
+        eprintln!("Failed to logout: {}", e);
+        process::exit(1);
+    }
 
     if let Err(e) = client.save_log() {
-		eprintln!("Failed to save log file: {}", e);
-		process::exit(1);
-	}
+        eprintln!("Failed to save log file: {}", e);
+        process::exit(1);
+    }
 }
 
 fn build_client() -> Client {
-	let server = get_input("Enter server IP or name: ");
+    let server = get_input("Enter server IP or name: ");
 
-	let port = get_input("Enter server port: ");
-	let port: u16 = port.parse().expect("Failed to convert port from String to u16");
+    let port = get_input("Enter server port: ");
+    let port: u16 = port.parse().expect("Failed to convert port from String to u16");
 
-	let mut client = Client::new(server.as_str(), port);
+    let mut client = Client::new(server.as_str(), port);
 
-	// NOT RECOMMENDED
-	// but setting this to true as this is an example
-	client.accept_invalid_certs(true);
+    // NOT RECOMMENDED
+    // but setting this to true as this is an example
+    client.accept_invalid_certs(true);
 
     client.log_file("discard_sessions.log");
 
-	client
+    client
 }
 
 fn get_input(msg: &str) -> String {
-	print!("{}", msg);
-	io::stdout().flush().expect("Failed to flush stdout buffer");
+    print!("{}", msg);
+    io::stdout().flush().expect("Failed to flush stdout buffer");
 
-	let mut s = String::new();
-	io::stdin().read_line(&mut s).expect("Failed to read from stdin");
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read from stdin");
 
-	s.trim().to_string()
+    s.trim().to_string()
 }
 
 fn login(client: &mut Client) -> Result<(), Error> {
-	let user = get_input("Enter username: ");
+    let user = get_input("Enter username: ");
 
-	print!("Enter password (will not be shown on screen): ");
-	io::stdout().flush().expect("Failed to flush stdout buffer");
-	let pass = rpassword::read_password().expect("Failed to read password");
+    print!("Enter password (will not be shown on screen): ");
+    io::stdout().flush().expect("Failed to flush stdout buffer");
+    let pass = rpassword::read_password().expect("Failed to read password");
 
-	println!("\n\nLogging into the API...\n");
+    println!("\n\nLogging into the API...\n");
 
-	let login_res = client.login(user.as_str(), pass.as_str())?;
+    let login_res = client.login(user.as_str(), pass.as_str())?;
 
-	if login_res.is_not_success() {
-		let msg = format!("{}", login_res.data["message"]);
-		return Err(Error::Custom(msg));
-	}
+    if login_res.is_not_success() {
+        let msg = format!("{}", login_res.data["message"]);
+        return Err(Error::Custom(msg));
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn logout(client: &mut Client) -> Result<(), Error> {
-	println!("Logging out...");
+    println!("Logging out...");
 
-	let logout_res = client.logout()?;
+    let logout_res = client.logout()?;
 
-	if logout_res.is_not_success() {
-		let msg = format!("{}", logout_res.data["message"]);
-		return Err(Error::Custom(msg));
-	}
+    if logout_res.is_not_success() {
+        let msg = format!("{}", logout_res.data["message"]);
+        return Err(Error::Custom(msg));
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn discard_sessions(client: &mut Client) -> Result<(), Error> {
-	println!("Querying all sessions...");
+    println!("Querying all sessions...");
 
-	let sessions_res = client.query("show-sessions", "full")?;
+    let sessions_res = client.query("show-sessions", "full")?;
 
-	if sessions_res.is_not_success() {
-		let msg = format!("{}", sessions_res.data["message"]);
-		return Err(Error::Custom(msg));
-	}
+    if sessions_res.is_not_success() {
+        let msg = format!("{}", sessions_res.data["message"]);
+        return Err(Error::Custom(msg));
+    }
 
     for session in sessions_res.objects {
         // only discard web_api sessions
@@ -135,5 +135,5 @@ fn discard_sessions(client: &mut Client) -> Result<(), Error> {
         println!("Failed to discard my own session");
     }
 
-	Ok(())
+    Ok(())
 }
--- examples/install_policy.rs
+++ examples/install_policy.rs
@@ -7,111 +7,111 @@ use std::process;
 use std::io::{self, Write};
 
 fn main() {
-	println!("Rust Management API Install Policy Example\n");
+    println!("Rust Management API Install Policy Example\n");
 
-	let mut client = build_client();
+    let mut client = build_client();
 
-	if let Err(e) = login(&mut client) {
-		eprintln!("Failed to login: {}", e);
-		process::exit(1);
-	}
+    if let Err(e) = login(&mut client) {
+        eprintln!("Failed to login: {}", e);
+        process::exit(1);
+    }
 
-	if let Err(e) = install(&mut client) {
-		eprintln!("Failed to install-policy: {}", e);
-		logout(&mut client).expect("Failed to logout");
-		process::exit(1);
-	}
+    if let Err(e) = install(&mut client) {
+        eprintln!("Failed to install-policy: {}", e);
+        logout(&mut client).expect("Failed to logout");
+        process::exit(1);
+    }
 
-	if let Err(e) = logout(&mut client) {
-		eprintln!("Failed to logout: {}", e);
-		process::exit(1);
-	}
+    if let Err(e) = logout(&mut client) {
+        eprintln!("Failed to logout: {}", e);
+        process::exit(1);
+    }
 
-	if let Err(e) = client.save_log() {
-		eprintln!("Failed to save log file: {}", e);
-		process::exit(1);
-	}
+    if let Err(e) = client.save_log() {
+        eprintln!("Failed to save log file: {}", e);
+        process::exit(1);
+    }
 }
 
 fn build_client() -> Client {
-	let server = get_input("Enter server IP or name: ");
+    let server = get_input("Enter server IP or name: ");
 
-	let port = get_input("Enter server port: ");
-	let port: u16 = port.parse().expect("Failed to convert port from String to u16");
+    let port = get_input("Enter server port: ");
+    let port: u16 = port.parse().expect("Failed to convert port from String to u16");
 
-	let mut client = Client::new(server.as_str(), port);
+    let mut client = Client::new(server.as_str(), port);
 
-	// NOT RECOMMENDED
-	// but setting this to true as this is an example
-	client.accept_invalid_certs(true);
+    // NOT RECOMMENDED
+    // but setting this to true as this is an example
+    client.accept_invalid_certs(true);
 
-	client.log_file("install_policy.log");
+    client.log_file("install_policy.log");
 
-	client
+    client
 }
 
 fn get_input(msg: &str) -> String {
-	print!("{}", msg);
-	io::stdout().flush().expect("Failed to flush stdout buffer");
+    print!("{}", msg);
+    io::stdout().flush().expect("Failed to flush stdout buffer");
 
-	let mut s = String::new();
-	io::stdin().read_line(&mut s).expect("Failed to read from stdin");
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read from stdin");
 
-	s.trim().to_string()
+    s.trim().to_string()
 }
 
 fn login(client: &mut Client) -> Result<(), Error> {
-	let user = get_input("Enter username: ");
+    let user = get_input("Enter username: ");
 
-	print!("Enter password (will not be shown on screen): ");
-	io::stdout().flush().expect("Failed to flush stdout buffer");
-	let pass = rpassword::read_password().expect("Failed to read password");
+    print!("Enter password (will not be shown on screen): ");
+    io::stdout().flush().expect("Failed to flush stdout buffer");
+    let pass = rpassword::read_password().expect("Failed to read password");
 
-	println!("\n\nLogging into the API...\n");
+    println!("\n\nLogging into the API...\n");
 
-	let login_res = client.login(user.as_str(), pass.as_str())?;
+    let login_res = client.login(user.as_str(), pass.as_str())?;
 
-	if login_res.is_not_success() {
-		let msg = format!("{}", login_res.data["message"]);
-		return Err(Error::Custom(msg));
-	}
+    if login_res.is_not_success() {
+        let msg = format!("{}", login_res.data["message"]);
+        return Err(Error::Custom(msg));
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn logout(client: &mut Client) -> Result<(), Error> {
-	println!("Logging out...");
+    println!("Logging out...");
 
-	let logout_res = client.logout()?;
+    let logout_res = client.logout()?;
 
-	if logout_res.is_not_success() {
-		let msg = format!("{}", logout_res.data["message"]);
-		return Err(Error::Custom(msg));
-	}
+    if logout_res.is_not_success() {
+        let msg = format!("{}", logout_res.data["message"]);
+        return Err(Error::Custom(msg));
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn install(client: &mut Client) -> Result<(), Error> {
-	println!("Going to install policy");
+    println!("Going to install policy");
 
-	let gateway = get_input("Enter Gateway/Cluster name: ");
-	let policy = get_input("Enter Policy name: ");
+    let gateway = get_input("Enter Gateway/Cluster name: ");
+    let policy = get_input("Enter Policy name: ");
 
-	let payload = json!({
-		"policy-package": policy,
-		"access": true,
-		"targets": gateway
-	});
+    let payload = json!({
+        "policy-package": policy,
+        "access": true,
+        "targets": gateway
+    });
 
-	println!("\nInstalling {} to {}\n", policy, gateway);
+    println!("\nInstalling {} to {}\n", policy, gateway);
 
-	let install_res = client.call("install-policy", payload)?;
+    let install_res = client.call("install-policy", payload)?;
 
-	if install_res.is_not_success() {
-		let msg = format!("{}", install_res.data["message"]);
-		return Err(Error::Custom(msg));
-	}
+    if install_res.is_not_success() {
+        let msg = format!("{}", install_res.data["message"]);
+        return Err(Error::Custom(msg));
+    }
 
-	Ok(())
+    Ok(())
 }
